39c83efb5c91fb67fd44dcfda06bb8bf93aea53b
jromero764 <85945162+jromero764@users.noreply.github.com>
2023-04-30 21:49:37 -0300

    Update inicializar.bat

--- inicializar.bat
+++ inicializar.bat
@@ -1,2 +1,4 @@
-git clone https://github.com/marrz-uy/backoffice-api
-git clone https://github.com/marrz-uy/backoffice-front
+#!/bin/bash
+git clone https://github.com/marrz-uy/backoffice-backend.git
+git clone https://github.com/marrz-uy/backoffice-front.git
+docker-compose up
